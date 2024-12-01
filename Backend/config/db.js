472bf1cf3f1d066fad84fd239b8c3ad1ac8fd2f3
Lavina <lavinaguglani456@gmail.com>
2024-12-01 22:49:43 +0530

    change

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const connectdb = async () => {
     try {
         // Attempt to connect to MongoDB using MONGO_URL from environment variables
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URL
+        );
         console.log("Database connected successfully!");
     } catch (error) {
         // Log detailed error message
